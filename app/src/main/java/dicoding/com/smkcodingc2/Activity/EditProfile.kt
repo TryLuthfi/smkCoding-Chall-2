71716eb7e7be3f20afbbd3c4da0a8864949e703b
TryLuthfi <ty01091990@gmail.com>
2020-05-16 20:13:52 +0700

    Test Commit Layout #01

--- app/src/main/java/dicoding/com/smkcodingc2/Activity/EditProfile.kt
+++ app/src/main/java/dicoding/com/smkcodingc2/Activity/EditProfile.kt
@@ -10,7 +10,7 @@ class EditProfile : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_profile)
-
+//p
         hideStatusBar()
     }
 
